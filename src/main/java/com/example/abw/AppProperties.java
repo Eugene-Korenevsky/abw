b8e372676ea5279c8fec0807f446f44477044e6a
Eugene Korenevsky <kara-91@tut.by>
2021-03-15 16:12:01 +0300

    feat: init properties

---
+++ src/main/java/com/example/abw/AppProperties.java
@@ -0,0 +1,14 @@
+package com.example.abw;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties
+@PropertySource("classpath:global.properties")
+public class AppProperties {
+    private int pageSize;
+}
