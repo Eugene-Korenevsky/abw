cfecc8e7127172fcab7d82fc4cbf7f7003a57044
Eugene Korenevsky <kara-91@tut.by>
2021-03-16 21:50:53 +0300

    feat: add method to repository

--- src/main/java/com/example/abw/repositories/pagination/ad/car_ad/CarAdPaginationRepository.java
+++ src/main/java/com/example/abw/repositories/pagination/ad/car_ad/CarAdPaginationRepository.java
@@ -5,13 +5,22 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public interface CarAdPaginationRepository extends PagingAndSortingRepository<CarAd, Long> {
     List<CarAd> readAllBySold(boolean sold, Pageable pageable);
 
-    public List<CarAd> findByCarBrand_CarBrandName_NameAndSold(String name,boolean sold, Pageable pageable);
+    public List<CarAd> findByCarBrand_CarBrandName_NameAndSold(String name, boolean sold, Pageable pageable);
 
-    public List<CarAd> findByCarBrand_NameAndSold(String name,boolean sold, Pageable pageable);
+    public List<CarAd> findByCarBrand_NameAndSold(String name, boolean sold, Pageable pageable);
+
+    List<CarAd> readAllByPriceBetweenAndSold(long start, long end, boolean sold, Pageable pageable);
+
+    List<CarAd> readAllByPriceBetweenAndSoldAndCarBrand_Name(long start, long end,
+                                                             boolean sold, String name, Pageable pageable);
+
+    List<CarAd> readAllByPriceBetweenAndSoldAndCarBrand_CarBrandName_Name(long start, long end,
+                                                             boolean sold, String name, Pageable pageable);
 }
