2cacc8b24129a308cbf148bdf8a2901716d5277d
Eugene Korenevsky <kara-91@tut.by>
2021-03-17 19:33:29 +0300

    feat: add ResourceNotFoundException

---
+++ src/main/java/com/example/abw/servicies/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.abw.servicies.exceptions;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
