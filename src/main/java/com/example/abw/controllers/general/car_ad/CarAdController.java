13534ee4c4350adfb405b3d8c38b3783f28f693e
Eugene Korenevsky <kara-91@tut.by>
2021-03-18 13:45:24 +0300

    feat: add controller for search

---
+++ src/main/java/com/example/abw/controllers/general/car_ad/CarAdController.java
@@ -0,0 +1,51 @@
+package com.example.abw.controllers.general.car_ad;
+
+import com.example.abw.entities.ad.Ad;
+import com.example.abw.entities.ad.CarAd;
+import com.example.abw.servicies.CarAdService;
+import com.example.abw.servicies.exceptions.ResourceNotFoundException;
+import com.example.abw.servicies.pagination.car_ad.simple.CarAdPaginationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/carAds", produces = "application/json")
+public class CarAdController {
+
+    @Autowired
+    private CarAdPaginationService defaultCarAdPagService;
+    @Autowired
+    private CarAdService carAdServiceImpl;
+
+    @GetMapping
+    public ResponseEntity<?> getAllCarAds(@RequestParam(value = "page", required = false) Integer page,
+                                          @RequestParam(value = "size", required = false) Integer size) {
+        if (page == null && size == null) {
+            List<Ad> ads = defaultCarAdPagService.getPaginationResultByDefault(0);
+            return new ResponseEntity<>(ads, HttpStatus.OK);
+        } else if (page == null) {
+            List<Ad> ads = defaultCarAdPagService.getPaginationResult(0, size);
+            return new ResponseEntity<>(ads, HttpStatus.OK);
+        } else if (size == null) {
+            List<Ad> ads = defaultCarAdPagService.getPaginationResultByDefault(page);
+            return new ResponseEntity<>(ads, HttpStatus.OK);
+        } else {
+            List<Ad> ads = defaultCarAdPagService.getPaginationResult(page, size);
+            return new ResponseEntity<>(ads, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCarAd(@PathVariable("id") long id) {
+        try {
+            CarAd carAd = carAdServiceImpl.findById(id);
+            return new ResponseEntity<>(carAd, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
+        }
+    }
+}
---
+++ src/main/java/com/example/abw/controllers/general/car_ad/search/ByCarBrandController.java
@@ -0,0 +1,71 @@
+package com.example.abw.controllers.general.car_ad.search;
+
+import com.example.abw.AppProperties;
+import com.example.abw.servicies.pagination.car_ad.SortKind;
+import com.example.abw.servicies.pagination.car_ad.with_one_param.CarAdPagServiceWithOneParam;
+import com.example.abw.servicies.response.car_ad.search.ResponseServiceWithOneParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/carAds")
+public class ByCarBrandController {
+    @Autowired
+    CarAdPagServiceWithOneParam carAdPagByCarBrand;
+    @Autowired
+    CarAdPagServiceWithOneParam carAdPagByCarBrandName;
+    @Autowired
+    private AppProperties appProperties;
+    @Autowired
+    private ResponseServiceWithOneParam responseServiceWithOneParamImpl;
+
+    @GetMapping("/carBrand")
+    public ResponseEntity<?> getAllByCarBrand(@RequestParam(value = "page") Integer page,
+                                              @RequestParam(value = "size", required = false) Integer size,
+                                              @RequestParam(value = "filter", required = false) String filter,
+                                              @RequestParam(value = "type", required = false) String type,
+                                              @RequestParam(value = "carBrand") String carBrand) {
+        if (type != null) {
+            if (type.equals("asc")) {
+                return responseServiceWithOneParamImpl.getResponseEntity(
+                        filter, size, carBrand, page, SortKind.ASC, carAdPagByCarBrand, appProperties);
+            } else if (type.equals("desc")) {
+                return responseServiceWithOneParamImpl.getResponseEntity(
+                        filter, size, carBrand, page, SortKind.DESC, carAdPagByCarBrand, appProperties);
+            } else {
+                return responseServiceWithOneParamImpl.getResponseEntity(
+                        filter, size, carBrand, page, SortKind.DESC, carAdPagByCarBrand, appProperties);
+            }
+        } else {
+            return responseServiceWithOneParamImpl.getResponseEntity(
+                    filter, size, carBrand, page, SortKind.DESC, carAdPagByCarBrand, appProperties);
+        }
+    }
+
+    @GetMapping("/carBrandName")
+    public ResponseEntity<?> getAllByCarBrandName(@RequestParam(value = "page") Integer page,
+                                                  @RequestParam(value = "size", required = false) Integer size,
+                                                  @RequestParam(value = "filter", required = false) String filter,
+                                                  @RequestParam(value = "type", required = false) String type,
+                                                  @RequestParam(value = "carBrandName") String carBrandName) {
+        if (type != null) {
+            if (type.equals("asc")) {
+                return responseServiceWithOneParamImpl.getResponseEntity(
+                        filter, size, carBrandName, page, SortKind.ASC, carAdPagByCarBrandName, appProperties);
+            } else if (type.equals("desc")) {
+                return responseServiceWithOneParamImpl.getResponseEntity(
+                        filter, size, carBrandName, page, SortKind.DESC, carAdPagByCarBrandName, appProperties);
+            } else {
+                return responseServiceWithOneParamImpl.getResponseEntity(
+                        filter, size, carBrandName, page, SortKind.DESC, carAdPagByCarBrandName, appProperties);
+            }
+        } else {
+            return responseServiceWithOneParamImpl.getResponseEntity(
+                    filter, size, carBrandName, page, SortKind.DESC, carAdPagByCarBrandName, appProperties);
+        }
+    }
+}
