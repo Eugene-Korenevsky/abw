827d815951a6ca63152fc4e2eacedacedafe0433
Eugene Korenevsky <kara-91@tut.by>
2021-03-18 18:56:01 +0300

    feat: add response serviceImpl

---
+++ src/main/java/com/example/abw/servicies/response/car_ad/search/logic/ResponseServiceByPrice.java
@@ -0,0 +1,8 @@
+package com.example.abw.servicies.response.car_ad.search.logic;
+
+import com.example.abw.servicies.response.car_ad.search.ResponseServiceWithTwoParam;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResponseServiceByPrice extends ResponseServiceWithTwoParam<Long, Long> {
+}
---
+++ src/main/java/com/example/abw/servicies/response/car_ad/search/logic/ResponseServiceByPriceAndString.java
@@ -0,0 +1,9 @@
+package com.example.abw.servicies.response.car_ad.search.logic;
+
+import com.example.abw.servicies.response.car_ad.search.ResponseServiceWithThreeParam;
+import com.example.abw.servicies.response.car_ad.search.ResponseServiceWithTwoParam;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResponseServiceByPriceAndString extends ResponseServiceWithThreeParam<Long, Long, String> {
+}
