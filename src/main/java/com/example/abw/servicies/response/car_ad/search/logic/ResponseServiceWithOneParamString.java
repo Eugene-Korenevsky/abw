c21002d29d99f2489d8b9d387343907ac6fa7782
Eugene Korenevsky <kara-91@tut.by>
2021-03-18 16:45:21 +0300

    refactor: change classes structure(use template method) as I understand it))

---
+++ src/main/java/com/example/abw/servicies/response/car_ad/search/logic/ResponseServiceWithOneParamString.java
@@ -0,0 +1,9 @@
+package com.example.abw.servicies.response.car_ad.search.logic;
+
+import com.example.abw.servicies.response.car_ad.search.ResponseServiceWithOneParam;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResponseServiceWithOneParamString extends ResponseServiceWithOneParam<String> {
+
+}
