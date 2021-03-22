3ee4b1c5dd72d26036aa3fd16da42dc157b4b7cd
Eugene Korenevsky <kara-91@tut.by>
2021-03-22 12:41:19 +0300

    feat: add admin controller

---
+++ src/main/java/com/example/abw/controllers/admin/car_ad/search/ByOneStringController.java
@@ -0,0 +1,72 @@
+package com.example.abw.controllers.admin.car_ad.search;
+
+import com.example.abw.AppProperties;
+import com.example.abw.servicies.pagination.car_ad.SortKind;
+import com.example.abw.servicies.pagination.car_ad.with_one_param.CarAdPagServiceWithOneParam;
+import com.example.abw.servicies.response.car_ad.search.ResponseServiceWithOneParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("adminByOneStringController")
+@RequestMapping("/admin/carAds")
+public class ByOneStringController {
+    @Autowired
+    private AppProperties appProperties;
+    @Autowired
+    CarAdPagServiceWithOneParam carAdPagByCarBrand;
+    @Autowired
+    CarAdPagServiceWithOneParam carAdPagByCarBrandName;
+
+    @Autowired
+    private ResponseServiceWithOneParam responseServiceWithOneParamStringAdmin;
+
+    @GetMapping("/carBrand")
+    public ResponseEntity<?> getAllByCarBrand(@RequestParam(value = "page") Integer page,
+                                              @RequestParam(value = "size", required = false) Integer size,
+                                              @RequestParam(value = "filter", required = false) String filter,
+                                              @RequestParam(value = "type", required = false) String type,
+                                              @RequestParam(value = "carBrand") String carBrand) {
+        if (type != null) {
+            if (type.equals("asc")) {
+                return responseServiceWithOneParamStringAdmin.getResponseEntity(
+                        filter, size, carBrand, page, SortKind.ASC, carAdPagByCarBrand, appProperties);
+            } else if (type.equals("desc")) {
+                return responseServiceWithOneParamStringAdmin.getResponseEntity(
+                        filter, size, carBrand, page, SortKind.DESC, carAdPagByCarBrand, appProperties);
+            } else {
+                return responseServiceWithOneParamStringAdmin.getResponseEntity(
+                        filter, size, carBrand, page, SortKind.DESC, carAdPagByCarBrand, appProperties);
+            }
+        } else {
+            return responseServiceWithOneParamStringAdmin.getResponseEntity(
+                    filter, size, carBrand, page, SortKind.DESC, carAdPagByCarBrand, appProperties);
+        }
+    }
+
+    @GetMapping("/carBrandName")
+    public ResponseEntity<?> getAllByCarBrandName(@RequestParam(value = "page") Integer page,
+                                                  @RequestParam(value = "size", required = false) Integer size,
+                                                  @RequestParam(value = "filter", required = false) String filter,
+                                                  @RequestParam(value = "type", required = false) String type,
+                                                  @RequestParam(value = "carBrandName") String carBrandName) {
+        if (type != null) {
+            if (type.equals("asc")) {
+                return responseServiceWithOneParamStringAdmin.getResponseEntity(
+                        filter, size, carBrandName, page, SortKind.ASC, carAdPagByCarBrandName, appProperties);
+            } else if (type.equals("desc")) {
+                return responseServiceWithOneParamStringAdmin.getResponseEntity(
+                        filter, size, carBrandName, page, SortKind.DESC, carAdPagByCarBrandName, appProperties);
+            } else {
+                return responseServiceWithOneParamStringAdmin.getResponseEntity(
+                        filter, size, carBrandName, page, SortKind.DESC, carAdPagByCarBrandName, appProperties);
+            }
+        } else {
+            return responseServiceWithOneParamStringAdmin.getResponseEntity(
+                    filter, size, carBrandName, page, SortKind.DESC, carAdPagByCarBrandName, appProperties);
+        }
+    }
+}
---
+++ src/main/java/com/example/abw/controllers/admin/car_ad/search/ByPriceController.java
@@ -0,0 +1,117 @@
+package com.example.abw.controllers.admin.car_ad.search;
+
+import com.example.abw.AppProperties;
+import com.example.abw.servicies.pagination.car_ad.SortKind;
+import com.example.abw.servicies.pagination.car_ad.with_three_param.CarAdPagServiceWithThreeParam;
+import com.example.abw.servicies.pagination.car_ad.with_two_param.CarAdPagServiceWithTwoParam;
+import com.example.abw.servicies.response.car_ad.search.ResponseServiceWithThreeParam;
+import com.example.abw.servicies.response.car_ad.search.ResponseServiceWithTwoParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("adminByPriceController")
+@RequestMapping("admin/carAds/price")
+public class ByPriceController {
+    @Autowired
+    private AppProperties appProperties;
+
+
+    @Autowired
+    private CarAdPagServiceWithTwoParam carAdPagBetweenPrice;
+    @Autowired
+    private CarAdPagServiceWithThreeParam carAdPagBetweenPriceAndCarBrand;
+    @Autowired
+    private CarAdPagServiceWithThreeParam carAdPagBetweenPriceAndCarBrandName;
+
+
+    @Autowired
+    private ResponseServiceWithTwoParam responseServiceByPriceAdmin;
+    @Autowired
+    private ResponseServiceWithThreeParam responseServiceByPriceAndStringAdmin;
+
+    @GetMapping
+    public ResponseEntity<?> getAllByPrice(@RequestParam(value = "page") Integer page,
+                                           @RequestParam(value = "size", required = false) Integer size,
+                                           @RequestParam(value = "filter", required = false) String filter,
+                                           @RequestParam(value = "type", required = false) String type,
+                                           @RequestParam(value = "startPrice") Long startPrice,
+                                           @RequestParam(value = "endPrice") Long endPrice) {
+        if (type != null) {
+            if (type.equals("asc")) {
+                return responseServiceByPriceAdmin.getResponseEntity(
+                        filter, size, startPrice, endPrice, page, SortKind.ASC, carAdPagBetweenPrice, appProperties);
+            } else if (type.equals("desc")) {
+                return responseServiceByPriceAdmin.getResponseEntity(
+                        filter, size, startPrice, endPrice, page, SortKind.DESC, carAdPagBetweenPrice, appProperties);
+            } else {
+                return responseServiceByPriceAdmin.getResponseEntity(
+                        filter, size, startPrice, endPrice, page, SortKind.DESC, carAdPagBetweenPrice, appProperties);
+            }
+        } else {
+            return responseServiceByPriceAdmin.getResponseEntity(
+                    filter, size, startPrice, endPrice, page, SortKind.DESC, carAdPagBetweenPrice, appProperties);
+        }
+    }
+
+    @GetMapping("/carBrand")
+    public ResponseEntity<?> getAllByPriceAndCarBrand(@RequestParam(value = "page") Integer page,
+                                                      @RequestParam(value = "size", required = false) Integer size,
+                                                      @RequestParam(value = "filter", required = false) String filter,
+                                                      @RequestParam(value = "type", required = false) String type,
+                                                      @RequestParam(value = "startPrice") Long startPrice,
+                                                      @RequestParam(value = "endPrice") Long endPrice,
+                                                      @RequestParam(value = "carBrand") String carBrand) {
+        if (type != null) {
+            if (type.equals("asc")) {
+                return responseServiceByPriceAndStringAdmin.getResponseEntity(
+                        filter, size, startPrice, endPrice, carBrand, page,
+                        SortKind.ASC, carAdPagBetweenPriceAndCarBrand, appProperties);
+            } else if (type.equals("desc")) {
+                return responseServiceByPriceAndStringAdmin.getResponseEntity(
+                        filter, size, startPrice, endPrice, carBrand, page,
+                        SortKind.DESC, carAdPagBetweenPriceAndCarBrand, appProperties);
+            } else {
+                return responseServiceByPriceAndStringAdmin.getResponseEntity(
+                        filter, size, startPrice, endPrice, carBrand, page,
+                        SortKind.DESC, carAdPagBetweenPriceAndCarBrand, appProperties);
+            }
+        } else {
+            return responseServiceByPriceAndStringAdmin.getResponseEntity(
+                    filter, size, startPrice, endPrice, carBrand, page,
+                    SortKind.DESC, carAdPagBetweenPriceAndCarBrand, appProperties);
+        }
+    }
+
+    @GetMapping("/carBrandName")
+    public ResponseEntity<?> getAllByPriceAndCarBrandName(@RequestParam(value = "page") Integer page,
+                                                          @RequestParam(value = "size", required = false) Integer size,
+                                                          @RequestParam(value = "filter", required = false) String filter,
+                                                          @RequestParam(value = "type", required = false) String type,
+                                                          @RequestParam(value = "startPrice") Long startPrice,
+                                                          @RequestParam(value = "endPrice") Long endPrice,
+                                                          @RequestParam(value = "carBrandName") String carBrandName) {
+        if (type != null) {
+            if (type.equals("asc")) {
+                return responseServiceByPriceAndStringAdmin.getResponseEntity(
+                        filter, size, startPrice, endPrice, carBrandName, page,
+                        SortKind.ASC, carAdPagBetweenPriceAndCarBrandName, appProperties);
+            } else if (type.equals("desc")) {
+                return responseServiceByPriceAndStringAdmin.getResponseEntity(
+                        filter, size, startPrice, endPrice, carBrandName, page,
+                        SortKind.DESC, carAdPagBetweenPriceAndCarBrandName, appProperties);
+            } else {
+                return responseServiceByPriceAndStringAdmin.getResponseEntity(
+                        filter, size, startPrice, endPrice, carBrandName, page,
+                        SortKind.DESC, carAdPagBetweenPriceAndCarBrandName, appProperties);
+            }
+        } else {
+            return responseServiceByPriceAndStringAdmin.getResponseEntity(
+                    filter, size, startPrice, endPrice, carBrandName, page,
+                    SortKind.DESC, carAdPagBetweenPriceAndCarBrandName, appProperties);
+        }
+    }
+}
