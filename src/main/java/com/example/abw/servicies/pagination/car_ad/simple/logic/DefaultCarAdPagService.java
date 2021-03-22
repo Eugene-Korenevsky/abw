07286ea01727e4780eaa8d03cfcdb6f1c65cb10d
Eugene Korenevsky <kara-91@tut.by>
2021-03-22 12:48:52 +0300

    fix: add pagination for admin

--- src/main/java/com/example/abw/servicies/pagination/car_ad/simple/logic/DefaultCarAdPagService.java
+++ src/main/java/com/example/abw/servicies/pagination/car_ad/simple/logic/DefaultCarAdPagService.java
@@ -2,6 +2,8 @@ package com.example.abw.servicies.pagination.car_ad.simple.logic;
 
 import com.example.abw.AppProperties;
 import com.example.abw.entities.ad.Ad;
+import com.example.abw.entities.ad.CarAd;
+import com.example.abw.entities.user.User;
 import com.example.abw.repositories.pagination.ad.car_ad.CarAdPaginationRepository;
 import com.example.abw.servicies.pagination.car_ad.simple.CarAdPaginationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -32,5 +35,25 @@ public class DefaultCarAdPagService implements CarAdPaginationService {
     public List<Ad> getPaginationResultByDefault(int page) {
         Pageable sortedByDate = PageRequest.of(page, appProperties.getPageSize(), Sort.by("publicationDate").descending());
         return new ArrayList<>(carAdPaginationRepository.readAllBySold(false, sortedByDate));
+    }
+
+    @Override
+    public List<Ad> getAdminPaginationResult(int page, int size) {
+        Pageable sortedByDate = PageRequest.of(page, size, Sort.by("publicationDate").descending());
+        return getResult(sortedByDate);
+    }
+
+    @Override
+    public List<Ad> getAdminPaginationResultByDefault(int page) {
+        Pageable sortedByDate = PageRequest.of(page, appProperties.getPageSize(), Sort.by("publicationDate").descending());
+        return getResult(sortedByDate);
+    }
+
+    private List<Ad> getResult(Pageable pageable) {
+        List<Ad> result = new ArrayList<>();
+        Iterable<CarAd> iterable = carAdPaginationRepository.findAll(pageable);
+        Iterator<CarAd> iterator = iterable.iterator();
+        if (iterator.hasNext()) iterator.forEachRemaining(result::add);
+        return result;
     }
 }
