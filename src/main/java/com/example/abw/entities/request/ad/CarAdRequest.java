5ec643a0d6ee63ff7a38beb8842b828e3dae42ca
Eugene Korenevsky <kara-91@tut.by>
2021-03-23 09:21:01 +0300

    feat: add field for images

--- src/main/java/com/example/abw/entities/request/ad/CarAdRequest.java
+++ src/main/java/com/example/abw/entities/request/ad/CarAdRequest.java
@@ -1,16 +1,20 @@
 package com.example.abw.entities.request.ad;
 
+import com.example.abw.entities.ad.image.Image;
 import com.example.abw.entities.sell_item.SellItem;
 import com.example.abw.entities.sell_item.car.CarBrand;
 import com.example.abw.entities.user.User;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.Set;
+
 @Data
 @EqualsAndHashCode
-public class CarAdRequest  {
+public class CarAdRequest {
     private long userId;
     private long carBrandId;
     private long price;
     private String descriptions;
+    private Set<String> images;
 }
