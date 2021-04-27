f16677c94661a934741258273aa7dd874ae2b055
Eugene Korenevsky <kara-91@tut.by>
2021-04-27 11:02:00 +0300

    refactor: do method in one line

--- src/main/java/com/example/abw/utils/currency/CurrencyUtil.java
+++ src/main/java/com/example/abw/utils/currency/CurrencyUtil.java
@@ -4,15 +4,13 @@ import com.example.abw.model.currency.Currency;
 import org.springframework.stereotype.Component;
 
 import java.util.EnumSet;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Component
 public class CurrencyUtil {
     public String getCurrencyString(Currency currentCurrency) {
-        Stream<Currency> stream = EnumSet.allOf(Currency.class).stream();
-        String url = stream.filter(x -> !x.equals(currentCurrency)).map(x -> x + ",")
-                .reduce("", (res, str) -> res + str);
-        int last = url.lastIndexOf(",");
-        return url.substring(0, last);
+        return EnumSet.allOf(Currency.class).stream().filter(x -> !x.equals(currentCurrency))
+                .map(Enum::toString).collect(Collectors.joining(","));
     }
 }
