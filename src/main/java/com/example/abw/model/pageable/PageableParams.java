511fe6ead88c9c0d61b5427813ff6efd87e7c2de
oadmin <kara-91@tut.by>
2021-05-25 10:58:23 +0300

    fix: add validation parameters to params

--- src/main/java/com/example/abw/model/pageable/PageableParams.java
+++ src/main/java/com/example/abw/model/pageable/PageableParams.java
@@ -3,12 +3,20 @@ package com.example.abw.model.pageable;
 import com.example.abw.model.currency.Currency;
 import com.example.abw.model.pageable.sort_kind.SortKind;
 import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 public class PageableParams {
+    @NonNull
     private int page;
+    @NonNull
     private int size;
+    @NotNull
     private String filter;
+    @NotNull
     private SortKind sortKind;
+    @NotNull
     private Currency currency;
 }
