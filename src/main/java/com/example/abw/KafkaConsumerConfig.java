4b89d127d5a8e932aa28f8d61570bfa779e44e1a
oadmin <kara-91@tut.by>
2021-06-09 11:39:40 +0300

    feat : add kafka

---
+++ src/main/java/com/example/abw/KafkaConsumerConfig.java
@@ -0,0 +1,40 @@
+package com.example.abw;
+
+import com.example.abw.entities.currency.CurrencyExchange;
+import com.example.abw.model.currency.CurrencyExchangeDTO;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    String bootstrapAddress = "localhost:9092";
+
+    public ConsumerFactory<String, CurrencyExchangeDTO> userConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "currency_exchange");
+        JsonDeserializer<CurrencyExchangeDTO> jsonDeserializer
+                = new JsonDeserializer<>(CurrencyExchangeDTO.class, false);
+        jsonDeserializer.addTrustedPackages("*");
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), jsonDeserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, CurrencyExchangeDTO> exchangeKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, CurrencyExchangeDTO> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(userConsumerFactory());
+        return factory;
+    }
+}
---
+++ src/main/java/com/example/abw/kafka_consumers/CurrencyExchangeListener.java
@@ -0,0 +1,28 @@
+package com.example.abw.kafka_consumers;
+
+
+import com.example.abw.entities.currency.CurrencyExchange;
+import com.example.abw.model.currency.CurrencyExchangeDTO;
+import com.example.abw.servicies.CurrencyExchangeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Component
+@EnableKafka
+public class CurrencyExchangeListener {
+    @Autowired
+    private CurrencyExchangeService currencyExchangeService;
+
+    private final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    @org.springframework.kafka.annotation.KafkaListener(topics = "currency_exchange",
+            containerFactory = "exchangeKafkaListenerContainerFactory")
+    public void currencyExchangeListening(CurrencyExchangeDTO currencyExchange) {
+        System.out.println("final currency " + currencyExchange);
+        currencyExchangeService.updateCurrencyExchange(currencyExchange);
+        this.countDownLatch.countDown();
+    }
+}
