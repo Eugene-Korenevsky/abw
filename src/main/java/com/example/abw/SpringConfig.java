db17ec71c756e18119e9ebe1e953a36badd6d1f9
oadmin <kara-91@tut.by>
2021-06-28 09:40:49 +0300

    feat : add object mapper

--- src/main/java/com/example/abw/SpringConfig.java
+++ src/main/java/com/example/abw/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.example.abw;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,4 +13,7 @@ public class SpringConfig {
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public ObjectMapper objectMapper() {return new ObjectMapper();}
 }
